a9dc74e21cd2273be1271309fdeba9085c2c2d30
umurutasate3 <claudeumurutasate4@gmail.com>
2024-06-11 11:10:57 +0200

    claude

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
